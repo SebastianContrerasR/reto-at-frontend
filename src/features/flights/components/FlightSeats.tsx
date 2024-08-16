f8ba9aaeb0492edc351885233d94a69d55ba2016
Sebastian Contreras <sstfmscontreras@gmail.com>
2024-08-15 20:33:53 -0500

    added seat icon to seats

--- src/features/flights/components/FlightSeats.tsx
+++ src/features/flights/components/FlightSeats.tsx
@@ -1,4 +1,5 @@
 import { Seat } from "@/features/flights/types/flight-details";
+import SeatIcon from "./SeatIcon";
 
 interface FlightSeatsProps {
     seats: Seat[];
@@ -7,32 +8,39 @@ interface FlightSeatsProps {
     selectedSeats: Seat[];
 }
 
+const getSeatIconClasses = (seat: Seat, isSelected: boolean) => {
+    if (seat.status === 'free') {
+        return `stroke-blue-500 fill-white hover:fill-blue-400 hover:stroke-white ${isSelected ? 'fill-blue-500 stroke-white' : ''
+            }`;
+    }
+
+    if (seat.status === 'booked') {
+        return 'stroke-white fill-gray-400';
+    }
+
+    return '';
+};
+
 export const FlightSeats: React.FC<FlightSeatsProps> = ({ seats, onSeatSelect, selectedSeats, isLoading }) => {
-    const getStatusClass = (status: 'free' | 'booked') => {
-        switch (status) {
-            case 'free':
-                return 'bg-blue-500 hover:bg-blue-600';
-            case 'booked':
-                return 'bg-gray-500 cursor-not-allowed opacity-70';
-            default:
-                return '';
-        }
-    };
 
     return (
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
-            {seats.map((seat) => (
-                <div key={seat.id} className="flex justify-center items-center">
-                    <button
-                        onClick={() => !isLoading && seat.status === 'free' && onSeatSelect(seat)}
-                        className={`p-4 text-white font-semibold border rounded-md ${getStatusClass(seat.status)} ${selectedSeats.find((s) => s.id === seat.id) ? 'ring-2 ring-blue-800 bg-blue-700' : ''}`}
-                        disabled={seat.status === 'booked' || isLoading}
-                    >
-                        {seat.code}
+            {seats.map((seat) => {
+                const isSelected = !!selectedSeats.find((s) => s.id === seat.id);
 
-                    </button>
-                </div>
-            ))}
+                return (
+                    <div key={seat.id} className="flex justify-center items-center">
+                        <button
+                            onClick={() => !isLoading && seat.status === 'free' && onSeatSelect(seat)}
+                            disabled={seat.status === 'booked' || isLoading}
+                        >
+                            <SeatIcon
+                                className={`transition-colors duration-100 ${getSeatIconClasses(seat, isSelected)}`}
+                            />
+                        </button>
+                    </div>
+                );
+            })}
         </div>
     );
 };
